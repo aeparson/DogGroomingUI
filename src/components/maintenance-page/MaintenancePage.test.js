da3b7f93d35350148e52d8bed742dc9ef3ee6add
scasavant <scasavant@catalyte.io>
2022-03-30 17:24:22 -0400

    Added test for sorting order

--- src/components/maintenance-page/MaintenancePage.test.js
+++ src/components/maintenance-page/MaintenancePage.test.js
@@ -42,4 +42,39 @@ describe('Maintenance Page Component Tests', () => {
     expect(screen.getAllByRole('columnheader').length).toBe(20);
     expect(screen.getAllByRole('row').length).toEqual(3);
   });
+
+  it('sorts products based on id', () => {
+    fetchProducts.mockImplementation((setProducts, setApiError) => {
+      setApiError(false);
+      setProducts([{ id: 5, price: 80.00 },
+        { id: 999999999, price: 80.00 },
+        { id: 0, price: 80.00 }]);
+    });
+    render(
+      <MaintenancePage />, container
+    );
+    const rows = screen.getAllByRole('row');
+    expect(rows[0].firstChild.textContent).toBe('ID');
+    expect(rows[1].firstChild.textContent).toBe('0');
+    expect(rows[2].firstChild.textContent).toBe('5');
+    expect(rows[3].firstChild.textContent).toBe('999999999');
+  });
+
+  // it('has a scrollbar', () => {
+  //   // Construct sample data
+  //   const products = new Array(5);
+  //   for (let id = 0; id < 5; id += 1) {
+  //     products.push({ id, price: 1.00 });
+  //   }
+  //   fetchProducts.mockImplementation((setProducts, setApiError) => {
+  //     setApiError(false);
+  //     setProducts([]);
+  //   });
+  //   render(
+  //     <MaintenancePage />, container
+  //   );
+  //   const table = screen.getByTestId('maintenance table');
+  //   table.scrollTop = 1;
+  //   expect(table.scrollTop).toBe(1);
+  // });
 });
