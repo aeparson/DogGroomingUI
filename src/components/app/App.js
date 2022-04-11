f293cef198496d0797e10f5311b10227a65fdbdd
Kaleigh Hawkins <khawkins@catalyte.io>
2022-04-10 18:24:16 -1000

    finally figured out where the toast container goes without Es Lint rage quitting

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductPage from '../product-page/ProductPage';
 import CheckoutPage from '../checkout-page/CheckoutPage';
 import ConfirmationPage from '../confirmation-page/ConfirmationPage';
 import MaintenancePage from '../maintenance-page/MaintenancePage';
 import Header from '../header/Header';
+
+toast.configure();
 
 /**
  * @name App
@@ -20,6 +24,17 @@ const App = () => (
       <Route exact path="/confirmation" render={() => <ConfirmationPage />} />
       <Route exact path="/maintenance" render={() => <MaintenancePage />} />
     </Switch>
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable={false}
+      pauseOnHover={false}
+    />
   </BrowserRouter>
 );
 
