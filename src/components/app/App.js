52fa99522577ae7047dc95bba1de72c1b3bda8c4
Kaleigh Hawkins <khawkins@catalyte.io>
2022-04-13 07:09:52 -1000

    pull sprint 2 merge

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductPage from '../product-page/ProductPage';
 import CheckoutPage from '../checkout-page/CheckoutPage';
@@ -9,6 +11,8 @@ import Header from '../header/Header';
 import Footer from '../footer/footer';
 import ProfilePage from '../profile-page/ProfilePage';
 
+toast.configure();
+
 /**
  * @name App
  * @returns component
@@ -16,6 +20,7 @@ import ProfilePage from '../profile-page/ProfilePage';
 const App = () => (
   <BrowserRouter>
     <Header />
+<<<<<<< HEAD
     <div id="content">
       <Switch>
         <Route exact path="/" render={() => <ProductPage />} />
@@ -26,6 +31,26 @@ const App = () => (
       </Switch>
     </div>
     <Footer />
+=======
+    <Switch>
+      <Route exact path="/" render={() => <ProductPage />} />
+      <Route exact path="/checkout" render={() => <CheckoutPage />} />
+      <Route exact path="/confirmation" render={() => <ConfirmationPage />} />
+      <Route exact path="/maintenance" render={() => <MaintenancePage />} />
+    </Switch>
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable={false}
+      pauseOnHover={false}
+      limit={1}
+    />
+>>>>>>> 7ab5064e62d17cc6a934702c1a0717f768c372d1
   </BrowserRouter>
 );
 
