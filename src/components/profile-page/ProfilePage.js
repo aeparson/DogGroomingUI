839c6760b071850cf70375820ff200e303f4326d
Arcadia Parson <aparson@catalyte.io>
2022-04-14 01:19:33 +1000

    nonworking code for profile page purchase history. Have to commit so I can update another branch.

--- src/components/profile-page/ProfilePage.js
+++ src/components/profile-page/ProfilePage.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './ProfilePage.css';
 
 /**
- * @name ProfilePage
+ * @name ProfileInfo
  * @description fetches user info from API and displays in a form
  * @return component
  */
-const ProfilePage = () => (
+const ProfileInfo = () => (
 
   <form className={styles.profileContainer}>
     <h2 className="title"> Your User Profile</h2>
@@ -26,4 +26,64 @@ const ProfilePage = () => (
     </div>
   </form>
 );
-export default ProfilePage;
+
+const PurchaseHistory = () => {
+  const [purchase, setPurchase] = useState([]);
+  const [apiError, setApiError] = useState(false);
+
+  useEffect(() => {
+    fetchUserPurchases(setPurchase, setApiError);
+  }),
+
+    <div style={styles} className="scrollable">
+      <table>
+        <thead>
+          <TableHeadings />
+        </thead>
+        <tbody>
+          {purchase.sort((productA, productB) => productA.purchaseDate - productB.purchaseDate)
+            .map((product) => <TableData key={product.purchaseDate} product={product} />)}
+        </tbody>
+      </table>
+    </div>;
+};
+
+const TableHeadings = () => (
+  <tr>
+    <th>Purchase Date</th>
+    <th>Total Price</th>
+    <th>Products Purchased</th>
+  </tr>
+);
+
+/**
+ *
+ * @param {string} dateString UTC Datestring from API (YYYY-MM-DDTHH:MM:SS.SSSSSS)
+ * @returns {string} Formatted date (YYYY-MM-DD)
+ */
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  // months are zero-indexed
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+/**
+ * @description a row of table data for purchase history
+ * @param {Object} purch Contains a purchase object
+ * @returns component
+ */
+const TableData = (purch) => {
+  const { purchase } = purch;
+  return (
+    <tr>
+      <td style={{ textAlign: 'right' }}>{purchase.price.toFixed(2)}</td>
+      <td style={{ textAlign: 'right' }}>{purchase.quantity}</td>
+      <td>{formatDate(purchase.purchaseDate)}</td>
+    </tr>
+  );
+};
+
+export default { ProfileInfo, PurchaseHistory };
---
+++ src/components/profile-page/columns.js
@@ -0,0 +1,16 @@
+const COLUMNS = [
+  {
+    Header: 'Purchase Date',
+    accessor: 'OrderDate'
+  },
+  {
+    Header: 'Total Price',
+    accessor: 'PurchasePrice'
+  },
+  {
+    Header: 'Products',
+    accessor: 'ViewProducts'
+  }
+];
+
+export default COLUMNS;
