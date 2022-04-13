d5c982f575a96fba7e4482042927d5f282ffdf16
scasavant <scasavant@catalyte.io>
2022-04-13 15:50:04 -0400

    Added some checkout page tests

--- src/components/checkout-page/CheckoutPage.test.js
+++ src/components/checkout-page/CheckoutPage.test.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { unmountComponentAtNode } from 'react-dom';
-import { render, screen } from '@testing-library/react';
-import { ToastContainer, toast } from 'react-toastify';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { toast } from 'react-toastify';
 import CheckoutPage from './CheckoutPage';
 import makePurchase from './CheckoutService';
 import { useCart } from './CartContext';
+import validatePurchase from './CheckoutValidation';
 
 jest.mock('./CheckoutService');
 jest.mock('./CartContext');
+jest.mock('./CheckoutValidation');
+jest.mock('react-toastify');
+const mockHistory = jest.fn();
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: mockHistory
+  })
+}));
+
 let container = null;
 
 toast.configure();
@@ -17,6 +28,23 @@ describe('Checkout Page Component Tests', () => {
     // setup a DOM element as a render target
     container = document.createElement('div');
     document.body.appendChild(container);
+
+    // Some default mocked functions
+    useCart.mockImplementation(() => React.useContext(
+      React.createContext({
+        state: {
+          products: [{
+            price: 1, title: '1', description: '1', quantity: 1
+          }]
+        }
+      })
+    ));
+    // eslint-disable-next-line no-unused-vars
+    makePurchase.mockImplementation((products, delivery, billing, creditCard) => Promise.resolve(
+      false
+    ));
+    // eslint-disable-next-line no-unused-vars
+    validatePurchase.mockImplementation((delivery, billing, credit) => [{}, {}]);
   });
 
   afterEach(() => {
@@ -26,29 +54,52 @@ describe('Checkout Page Component Tests', () => {
     container = null;
   });
 
-  it('shows error toast when api fails', () => {
-    // eslint-disable-next-line no-unused-vars
-    makePurchase.mockImplementation((products, delivery, billing, creditCard) => false);
-    useCart.mockImplementation(() => {
-      React.useContext(React.createContext({ state: { products: [] } }));
-    });
+  it('shows error toast when api fails', async () => {
+    const toastCalls = [];
+    toast.error.mockImplementation((text) => { toastCalls.push(text); });
+
     render(
-      <>
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss={false}
-          draggable={false}
-          pauseOnHover={false}
-          limit={1}
-        />
-        <CheckoutPage />
-      </>, container
+      <CheckoutPage />, container
     );
-    expect(screen.findByText('Transaction could not be processed')).toBeInTheDocument();
+
+    await act(async () => {
+      userEvent.click(screen.getByText('Checkout'));
+    });
+
+    expect(toastCalls).toEqual(['Transaction could not be processed']);
+  });
+
+  it('redirects when transaction is successful', async () => {
+    // eslint-disable-next-line no-unused-vars
+    makePurchase.mockImplementation((products, delivery, billing, creditCard) => Promise.resolve(
+      {} // Successful transaction returns an object
+    ));
+
+    render(
+      <CheckoutPage />, container
+    );
+    await act(async () => {
+      userEvent.click(screen.getByText('Checkout'));
+    });
+
+    expect(mockHistory).toHaveBeenCalledWith('/confirmation');
+  });
+
+  it('neither toasts nor redirects when form is invalid', async () => {
+    // eslint-disable-next-line no-unused-vars
+    validatePurchase.mockImplementation((delivery, billing, credit) => [{ field: 'message' }, {}]); // Fake error
+    const toastCalls = [];
+    toast.error.mockImplementation((text) => { toastCalls.push(text); });
+
+    render(
+      <CheckoutPage />, container
+    );
+
+    await act(async () => {
+      userEvent.click(screen.getByText('Checkout'));
+    });
+
+    expect(toastCalls).toEqual([]);
+    expect(mockHistory).not.toHaveBeenCalled();
   });
 });
