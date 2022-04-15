a2d01545e3cb889824536d05e0f6e6c0550ec36f
Allison Goodrum <agoodrum@catalyte.io>
2022-04-15 09:59:17 -0400

    fix bug currentPage event target bug

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -3,11 +3,12 @@ import React, { useState, useEffect } from 'react';
 import './Pagination.css';
 
 const Pagination = ({
-  setPage, pageLimit, products, dataLimit
+  setPage, pageLimit
 }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [pages] = useState(Math.round(products.length / dataLimit));
+  // const [pages] = useState(Math.round(products.length / dataLimit));
 
+  // scroll to top
   useEffect(() => {
     window.scrollTo({ behavior: 'smooth', top: '0px' });
   }, [currentPage]);
@@ -27,7 +28,7 @@ const Pagination = ({
   function changePage(event) {
     const pageNumber = Number(event.target.textContent);
     setCurrentPage(pageNumber);
-    setPage(currentPage);
+    setPage(pageNumber - 1);
   }
   // make a group of pages
   const getPaginationGroup = () => {
@@ -70,7 +71,7 @@ const Pagination = ({
           type="button"
           onClick={goToNextPage}
           icon="right arrow"
-          className={`next ${currentPage === pages ? 'disabled' : ''}`}
+          className={`next ${currentPage === 27 ? 'disabled' : ''}`}
         >
           →
         </button>
