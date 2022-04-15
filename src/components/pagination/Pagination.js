9e476db3855e6e367b6d8521d36ef85eb2686a6a
Allison Goodrum <agoodrum@catalyte.io>
2022-04-15 09:32:37 -0400

    add scroll to top after pagination

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { Icon } from 'semantic-ui-react';
 import './Pagination.css';
 
 const Pagination = ({
-  setPage, pageLimit
+  setPage, pageLimit, products, dataLimit
 }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  // const [pages] = useState(Math.round(products.length / dataLimit));
+  const [pages] = useState(Math.round(products.length / dataLimit));
+
+  useEffect(() => {
+    window.scrollTo({ behavior: 'smooth', top: '0px' });
+  }, [currentPage]);
 
   // increment the current page
   function goToNextPage() {
@@ -66,7 +70,7 @@ const Pagination = ({
           type="button"
           onClick={goToNextPage}
           icon="right arrow"
-          className={`next ${currentPage === 27 ? 'disabled' : ''}`}
+          className={`next ${currentPage === pages ? 'disabled' : ''}`}
         >
           →
         </button>
