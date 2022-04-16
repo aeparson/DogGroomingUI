947c8fd4dd2a08ab547790512777e9dd4e96a617
Allison Goodrum <agoodrum@catalyte.io>
2022-04-16 16:06:40 -0400

    works on paginatedGroup, unsuccessfully

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
-// import { Icon } from 'semantic-ui-react';
+import React, { useState } from 'react';
 import './Pagination.css';
 
 const Pagination = ({
-  setPage, pageLimit
+  setPage, dataLimit, pageLimit
 }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  // const [pages] = useState(Math.round(products.length / dataLimit));
+  const [totalProducts] = useState(1);
+  const [setPages] = useState(Math.round(totalProducts?.length / dataLimit));
+
   // scroll to top
-  useEffect(() => {
-    window.scrollTo({ behavior: 'smooth', top: '0px' });
-  }, [currentPage]);
+  // useEffect(() => {
+  //   window.scrollTo({ behavior: 'smooth', top: '0px' });
+  // }, [currentPage]);
+
   // increment the current page
   function goToNextPage() {
     setCurrentPage(currentPage + 1);
@@ -30,6 +32,7 @@ const Pagination = ({
   // make a group of pages
   const getPaginationGroup = () => {
     const start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+    // const dynamicProductAmount = setTotalProducts?.Length / dataLimit;
     return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
   };
   return (
@@ -66,7 +69,7 @@ const Pagination = ({
           type="button"
           onClick={goToNextPage}
           icon="right arrow"
-          className={`next ${currentPage === 27 ? 'disabled' : ''}`}
+          className={`next ${currentPage === setPages?.length ? 'disabled' : ''}`}
         >
           →
         </button>
