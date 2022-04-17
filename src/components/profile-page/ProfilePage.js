dc4c1f7304003e4bbdbe7acece768fba46a2bb10
Abigail Jokerst <ajokerst@catalyte.io>
2022-04-16 19:58:13 -0600

    creates test page

--- src/components/profile-page/ProfilePage.js
+++ src/components/profile-page/ProfilePage.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import styles from './ProfilePage.css';
 import Constants from '../../utils/constants';
-// import loginUser from '../header/HeaderService';
 
 import './ProfilePageService';
 
 /**
  * @name ProfilePage
- * @description fetches user info from API and displays in a form
- * @param {user} props Contains a user object
+ * @description displays in a form of logged in user data
+ * @param {user} props contains a user object
  * @return component
  */
 
@@ -29,80 +28,65 @@ const ProfilePage = ({ user }) => {
         </p>
         )}
         <div className="profileContainer">
-          <h2 className="title">
-            User Profile
-            <hr />
-          </h2>
-          {/* {userData.map((user) => <TableData key={user.email} googleUser={user} />)} */}
-          <h3 style={styles} className="underline">
-            Name
-          </h3>
-          <h4>
-            First Name:
-            {' '}
-            <span className="input">
-              {user.firstName}
-            </span>
-          </h4>
-          <h4>
-            Last Name:
-            {' '}
-            <span className="input">
-              {user.lastName}
-            </span>
-            <hr />
-          </h4>
-          <h3 style={styles} className="underline">
-            Address
-          </h3>
-          <h4>
-            Street:
-            {' '}
-            <span className="input">
-              {user.street}
-            </span>
-          </h4>
-          <h4>
-            City:
-            {' '}
-            <span className="input">
-              {user.city}
-            </span>
-          </h4>
-          <h4>
-            State:
-            {' '}
-            <span className="input">
-              {user.state}
-            </span>
-          </h4>
-          <h4>
-            Zip:
-            {' '}
-            <span className="input">
-              {user.zip}
-            </span>
-          </h4>
+          <p data-testid="createProfilePage">
+            <h2 className="title">
+              User Profile
+              <hr />
+            </h2>
+            <h3 style={styles} className="underline">
+              Name
+            </h3>
+            <h4>
+              First Name:
+              {' '}
+              <span className="input">
+                {user.firstName}
+              </span>
+            </h4>
+            <h4>
+              Last Name:
+              {' '}
+              <span className="input">
+                {user.lastName}
+              </span>
+              <hr />
+            </h4>
+            <h3 style={styles} className="underline">
+              Address
+            </h3>
+            <h4>
+              Street:
+              {' '}
+              <span className="input">
+                {user.street}
+              </span>
+            </h4>
+            <h4>
+              City:
+              {' '}
+              <span className="input">
+                {user.city}
+              </span>
+            </h4>
+            <h4>
+              State:
+              {' '}
+              <span className="input">
+                {user.state}
+              </span>
+            </h4>
+            <h4>
+              Zip:
+              {' '}
+              <span className="input">
+                {user.zip}
+              </span>
+            </h4>
+          </p>
         </div>
       </div>
     </>
   );
 };
 
-/**
- * @description a row of table data for user data
- * @param {Object} props Contains a user object
- * @returns component
- */
-// const TableData = (props) => {
-// const { user } = props;
-// return (
-//     <tr>
-//       <td style={{ textAlign: 'right' }}>{user.email}</td>
-//       <td>{user.FirstName}</td>
-//       <td>{user.LastName}</td>
-//       <td>{user.Street}</td>
-//     </tr>
-//   );
-// };
 export default ProfilePage;
