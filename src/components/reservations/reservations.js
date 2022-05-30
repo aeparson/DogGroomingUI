a433fffdd935a39186cf14934a6605646abe898a
Arcadia Parson <aparson@catalyte.io>
2022-05-30 16:31:32 +1000

    have a table displaying with styled table headers

--- src/components/reservations/reservations.js
+++ src/components/reservations/reservations.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './reservations.module.css';
+import TableHeadings from './reservationTableHeadings';
 
 const Reservations = () => (
-  <div className={styles.create}>
-    <NavLink to="/reservations/create">
-      <button className={styles.button} type="button">Create</button>
-    </NavLink>
-  </div>
+  <>
+    <div className={styles.create}>
+      <NavLink to="/reservations/create">
+        <button className={styles.button} type="button">Create</button>
+      </NavLink>
+    </div>
+    <div className={styles.reservationTable}>
+      <table>
+        <TableHeadings />
+      </table>
+    </div>
+  </>
 
 );
 
---
+++ src/components/reservations/reservationsService.js
@@ -0,0 +1,67 @@
+import HttpHelper from '../../utils/HttpHelper';
+import Constants from '../../utils/constants';
+
+/**
+ *
+ * @name fetchAllReservations
+ * @description Utilizes HttpHelper to make a get request to an API
+ * @param {*} setReservations sets state for reservations
+ * @param {*} setApiError sets error if response other than 200 is returned
+ * @returns sets state for reservations if 200 response, else sets state for apiError
+ */
+async function fetchAllReservations(setReservations, setApiError) {
+  await HttpHelper(Constants.RESERVATIONS_ENDPOINT, 'GET')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(Constants.API_ERROR);
+    })
+    .then(setReservations)
+    .catch(() => {
+      setApiError(true);
+    });
+}
+
+/**
+ *
+ * @name deleteReservationById
+ * @description Utilizes HttpHelper to make a DELETE request to an API
+ * @param {int} reservationId id of reservation to be deleted
+ * @returns a deleted reservation or throws an error
+ */
+async function deleteReservationById(reservationId) {
+  await HttpHelper(`${Constants.RESERVATIONS_ENDPOINT}/${reservationId}`, 'DELETE')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      if (response.status === 400) {
+        throw new Error('Reservation has reviews');
+      }
+      throw new Error(Constants.API_ERROR);
+    });
+}
+
+/**
+ * @name updateReservationById
+ * @description Utilizes HttpHelper to make a PUT request to an API
+ * @param {int} reservationId
+ * @param {object} updatedReservation object passed from front end form elements.
+ */
+async function updateReservationById(reservation, updatedReservation) {
+  await HttpHelper(`${Constants.RESERVATIONS_ENDPOINT}/${reservation.id}`, 'PUT', updatedReservation)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      if (response.status === 400) {
+        throw new Error('A server error occurred. Your updates have not been saved');
+      }
+      throw new Error(Constants.API_ERROR);
+    });
+}
+
+export {
+  fetchAllReservations, deleteReservationById, updateReservationById
+};
