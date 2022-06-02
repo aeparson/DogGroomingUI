b4f23566ddfa0e817a3132f2749ef8c8ab882ff7
Arcadia Parson <aparson@catalyte.io>
2022-06-02 13:39:50 +1000

    created service page for roomTypes

--- src/components/roomtypes/roomtypes.js
+++ src/components/roomtypes/roomtypes.js
@@ -1,13 +1,151 @@
-import React from 'react';
-import { PLACEHOLDER_IMAGE } from '../../utils/constants';
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 import styles from './roomtypes.module.css';
+import fetchAllRoomTypes from './roomtypesService';
+import Constants from '../../utils/constants';
 
-const Roomtypes = () => (
-  <div>
-    <p>Room Types</p>
-    <img src={PLACEHOLDER_IMAGE} alt="luxurious dog hotel" className={styles.homepageImage} />
-  </div>
+const RoomTypes = () => {
+  const [reservations, setReservations] = useState([]);
+  const [apiError, setApiError] = useState(false);
+  const [roomType, setRoomType] = useState([]);
 
-);
+  const updateReservationList = () => getAllReservations(setReservations, setApiError);
+  const fetchRoomTypes = () => fetchAllRoomTypes(setRoomType, setApiError);
 
-export default Roomtypes;
+  useEffect(() => {
+    updateReservationList(setReservations);
+  }, []);
+
+  useEffect(() => {
+    fetchRoomTypes();
+  }, []);
+
+  const getRoomRate = (reservation) => {
+    const roomObject = roomType.find((object) => (object.id === reservation.id));
+    if (roomObject === undefined) {
+      return undefined;
+    }
+    const totalRate = ((roomObject.rate) * (reservation.numberOfNights)).toFixed(2);
+    return totalRate;
+  };
+
+  const getRoomTypeName = (reservation) => {
+    const roomObject = roomType.find((object) => (object.id === reservation.id));
+    if (roomObject === undefined) {
+      return undefined;
+    }
+    return roomObject.name;
+  };
+
+  return (
+    <>
+      {apiError && (
+      <p data-testid="errMsg">
+        {Constants.API_ERROR}
+      </p>
+      )}
+      <div className={styles.create}>
+        <NavLink to="/reservations/create">
+          <button className={styles.button} type="button">Create</button>
+        </NavLink>
+      </div>
+      <div className={styles.reservationTable}>
+        <table>
+          <thead>
+            <TableHeadings />
+          </thead>
+          <tbody>
+            {reservations.sort((reservationA, reservationB) => reservationA.id - reservationB.id)
+              .map((reservation) => (
+                <TableData
+                  key={reservation.id}
+                  updateReservations={updateReservationList}
+                  reservation={reservation}
+                  roomRate={getRoomRate(reservation)}
+                  roomName={getRoomTypeName(reservation)}
+                />
+              ))}
+
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+/**
+ * @description a row of table data for a reservation
+ * @param reservation will pull all of the reservations
+* @returns component
+ */
+
+const TableData = ({
+  reservation, roomRate, updateReservations, roomName
+}) => {
+  /**
+   * @description displays a pencil icon. When clicked, you are redirected to a page to edit a reservation.
+   * @returns a pencil icon.
+   */
+  const EditButton = () => {
+    const navigate = useNavigate();
+    const routeChange = () => {
+      const path = `/reservations/edit/${reservation.id}`;
+      navigate(path);
+    };
+
+    return (
+      <FontAwesomeIcon
+        onClick={routeChange}
+        icon={faPencil}
+        size="2x"
+        className={styles.leftButton}
+      />
+    );
+  };
+
+  /**
+   * @description displays a pencil icon. When clicked, you are redirected to a page to edit a reservation.
+   * @returns a pencil icon.
+   */
+  const DeleteButton = () => {
+    const onClick = () => {
+      deleteReservationById(reservation.id)
+        .then(() => {
+          updateReservations();
+        })
+        .catch(() => {
+          updateReservations();
+        });
+    };
+    return (
+      <FontAwesomeIcon
+        onClick={onClick}
+        icon={faTrash}
+        size="2x"
+        className={styles.rightButton}
+      />
+    );
+  };
+
+  return (
+    <tr>
+      <td>
+        <EditButton />
+        <DeleteButton
+          reservation={reservation}
+          updateReservations={updateReservations}
+        />
+
+      </td>
+      <td>{reservation.guestEmail}</td>
+      <td>{roomName}</td>
+      <td>{reservation.checkInDate}</td>
+      <td>{reservation.numberOfNights}</td>
+      <td>{`$${roomRate}`}</td>
+    </tr>
+  );
+};
+export default RoomTypes;
