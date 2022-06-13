46996c0df8d5273fa6066023c6108b03201e60c9
Arcadia Parson <aparson@catalyte.io>
2022-06-13 16:48:39 +1000

    rough replace of colors

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
-import companyLogo from './hotel-header-image.png';
+import companyLogo from './health-header-image.png';
 /**
  * @name Header
  * @description Displays the navigation header
